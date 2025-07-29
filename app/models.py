e119a914095e1d14f0a35ac5f789516fd5d07629
Naman Muley <naman.g.muley@gmail.com>
2025-07-30 01:40:47 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    name = Column(String, nullable=False)
